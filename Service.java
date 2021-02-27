fcac4f2ae47e108897db42438931d42a1d1bcc80
Agronom <Utarbaev2003@mail.ru>
2021-02-27 15:34:53 +0600

    pagination

--- Service.java
+++ Service.java
@@ -1,29 +1,22 @@
 package com.steamnonesteam.service;
-
 import com.j256.ormlite.dao.Dao;
 import com.steamnonesteam.model.Model;
-import org.omg.CORBA.portable.ApplicationException;
-
 import java.sql.SQLException;
 import java.util.List;
-
 public interface Service<T extends Model>{
     Dao<T,Integer> dao();
     default List<T> findAll() throws SQLException {
         return dao().queryForAll();
     }
-
     default T findById(int id) throws SQLException {
         return dao().queryForId(id);
     }
     default List<T> findBy(String columnName, Object value) throws SQLException {
         return dao().queryForEq(columnName, value);
     }
-
     default void save(T model) throws SQLException {
         dao().create(model);
     }
-
     default void update(T model) throws SQLException {
         dao().update(model);
     }
